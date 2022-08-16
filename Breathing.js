b8ef77e6a2ba772d107b0cb8ba78df15bc1b358b
Quentin Helvie <73959954+helvieq499@users.noreply.github.com>
2022-08-16 14:02:45 -0400

    Update Breathing.js

--- Breathing.js
+++ Breathing.js
@@ -5,19 +5,20 @@
 // @grant       none
 // @version     1.0
 // @author      helvieq499
-// @description Adjusts the body margin with time
+// @description Adjusts the top margin of the body tith time
+// @downloadURL https://raw.githubusercontent.com/helvieq499/UserScripts/master/Breathing.js
 // ==/UserScript==
 
 window.addEventListener("load", () => {
-  // disable the sidebars since those can resize the page
+  // disable the vertical sidebar since it can resize the page and cause text wrapping
   let style = document.createElement("style");
-  style.textContent = "body::-webkit-scrollbar { height: 0px }";
+  style.textContent = "body::-webkit-scrollbar { width: 0px }";
   document.head.appendChild(style);
   
   let original = document.body.style.margin || "0px";
   let render;
   (render = () => {
     requestAnimationFrame(render);
-    document.body.style.margin = `calc(${original} + ${Math.floor(Math.sin(Date.now() / 1000) * 10)}px)`
+    document.body.style.marginTop = `calc(${original} + ${Math.floor(Math.sin(Date.now() / 1000) * 10)}px)`
   })();
 });
